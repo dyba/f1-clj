beed1ef1f8e27a599fad93e005a1a63ef67ad156
Daniel Dyba <daniel.dyba@gmail.com>
2015-07-19 12:27:26 -0700

    Version 0.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.danieldyba/fellowship-one "0.0.4-SNAPSHOT"
+(defproject com.danieldyba/fellowship-one "0.0.4"
   :description "An API wrapper for Fellowship One"
   :url "https://github.com/dyba/fellowship-one"
   :license {:name "Eclipse Public License"
